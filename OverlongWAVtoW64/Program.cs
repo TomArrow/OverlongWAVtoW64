cf2f4ef58a37e232d3f2940c0734908a8816cf53
Tom <tomarrowtomarrow@hotmail.com>
2023-11-26 05:24:26 +0100

    Add project files.

---
+++ OverlongWAVtoW64/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OverlongWAVtoW64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
